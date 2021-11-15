8c55b123cc5a4d2780cca22194aad0f0b08883f9
LyNhanQuan <50731890+LyNhanQuan@users.noreply.github.com>
2021-11-15 21:30:40 +0700

    new upload - add laravel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,16 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
      * Bootstrap any application services.
      *
      * @return void
@@ -25,5 +15,14 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-}
 
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
