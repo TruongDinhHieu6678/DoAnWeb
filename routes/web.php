d875eb405665b9cd9da6a978d19b947661c61679
HIẾU TRƯƠNG ĐÌNH <18600090@student.hcmus.edu.vn>
2021-11-15 14:27:27 +0700

    demo controller va trang chu

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index'])->name('home.index');
+
+Route::get('/Trang-Chu',[HomeController::class,'index'])->name('home.index');
