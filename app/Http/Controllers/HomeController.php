2f786d7b9dbeec8686de4015269b229792f12046
LyNhanQuan <50731890+LyNhanQuan@users.noreply.github.com>
2021-11-27 20:21:36 +0700

    new login,logout,register

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 //các thư viện cho myspl truy vấn
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redirect;
 use DB;
 session_start();
 
@@ -11,15 +12,11 @@ use Illuminate\Http\Request;
 class HomeController extends Controller //class đc tạo để điều hướng
 {
     public function index(){
-        $Danhmuc_sp = DB::table('loaisanpham')->get();   
-         $SanPham = DB::table('sanpham')->paginate(6);  
-         $HangSX = DB::table('hangsanxuat')->get();
+        $Danhmuc_sp = DB::table('loaisanpham')->get();
+        $SanPham = DB::table('sanpham')->limit(6)->get();
+         $SanPham1 = DB::table('sanpham')->limit(3)->get();
         //$Danhmuc_sp = DB::table('loaisanpham')->orderby('MaloaiSanPham','desc')->get();
-        return view('page.Home')->with('all_SP',$SanPham)->with('loaisp',$Danhmuc_sp)->with('all_SX',$HangSX);//name page home thuộc Home.blade.php 
+        return view('page.Home')->with('loaisp',$Danhmuc_sp)->with('all_SP',$SanPham)->with('all_SP1',$SanPham1);//name page home thuộc Home.blade.php 
     }
-    //public function phantrang(){
-    //    $phan_trang=DB::table('sanpham')->paginate(6);
-   //     return view('page.Home')->with('pt',$phan_trang);
    
-  
 }
