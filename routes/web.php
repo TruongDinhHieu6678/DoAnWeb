a929b829bc41a8f8ee6362ce58d6b5b859413b86
LyNhanQuan <50731890+LyNhanQuan@users.noreply.github.com>
2021-12-11 10:57:59 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -11,23 +11,55 @@
 |
 */
 
-
+//trang chu
 Route::get('/','HomeController@index' );
 
+
 Route::get('/trang-chu','HomeController@index' );//trả về hàm HomeController với đường dẫn có /trang-chu
-    
-Route::get('/Login','LoginController@index' );
+Route::get('/Trang-Chu','HomeController@index' );
 
 
+//danh mục sp
+Route::get('/danh-muc-sp/{MaLoaiSanPham}','ProductController@show_danh_muc' );
+//hang sp
+Route::get('/thuong-hieu-sp/{MaHangSanXuat}','ProductController@show_thuong_hieu' );
+Route::get('/chi-tiet-sp/{MaSanPham}','ProductController@show_chi_tiet_sp' );
+
+
+//trang login
+Route::get('/Login-page','LoginController@index' );
+
+Route::post('/Trang-Chu','LoginController@login_user' );
+Route::post('/Admin-page','LoginController@login_admin' );
+
+Route::get('/Login-page','LoginController@logout' );
+
+
+//đăng ký
+Route::get('/dang-ky','RegisterController@index');
+Route::post('/trang-chu','RegisterController@add_user');
+
+//user page
+
+Route::get('/User-page','UserController@index' );
+
+
+Route::get('/Edit-page','UserController@edit');
+Route::post('/User-page','UserController@upload');
+Route::get('Product-Interest-page','UserController@Product_Interest');
+
+
+
+
+//admin page
 Route::get('/Admin-page','AdminController@index' );// cùng 1 page index là khung
 
-Route::get('/Admin-page','AdminController@dasboard_admin' );//nội dung
+//trang giỏ hàng
+Route::get('/Cart-page','CartController@index' );
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
-
-//Route::get('/trang-chu', function () {
-//    return view('welcome');
-//});
+//send mail
+Route::get('/Mail-page','HomeController@mail' );
+Route::post('/Forgot-Password','ForgotPasswordController@reset');
+Route::get('/Forgot-Password','ForgotPasswordController@index');
+Route::get('/update-new-pass','ForgotPasswordController@Update_New_Password');
+Route::post('/Login-page','ForgotPasswordController@Update_Pass');
