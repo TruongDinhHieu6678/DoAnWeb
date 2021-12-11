11be6e13a73eb0636b85c2085827060164200be0
HIẾU TRƯƠNG ĐÌNH <18600090@student.hcmus.edu.vn>
2021-12-11 21:52:08 +0700

    them controller account

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use Session;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Redirect;
+Session_start();
+
+
+class AccountController extends Controller
+{
+    public function list_account($page_focus)
+    {
+        //SELECT sp.TenSanPham, sp.GiaSanPham, sp.NgayNhap, sp.SoLuongTon, lsp.TenLoaiSanPham, 
+        //hsx.TenHangSanXuat FROM sanpham sp 
+        //JOIN loaisanpham lsp on sp.MaLoaiSanPham = lsp.MaLoaiSanPham 
+        //JOIN hangsanxuat hsx ON sp.MaHangSanXuat = hsx.MaHangSanXuat
+        $all_account = DB::table('taikhoan')->where('MaLoaiTaiKhoan',1)
+        ->orderBy('taikhoan.MaTaiKhoan', 'asc')
+        ->get();
+        $Max_page = count($all_account) % 5 == 0 ? count($all_account) / 5: ((count($all_account) / 5)+1);
+        $view_account = DB::table('taikhoan')->where('MaLoaiTaiKhoan',1)
+        ->orderBy('taikhoan.MaTaiKhoan', 'asc')
+        ->skip($page_focus*5-5)
+        ->take($page_focus*5)
+        ->get();
+        Session::put('page_focus', $page_focus);
+        $manager_account = view('admin.list_account')->with('all_account',$view_account)->with('Max_page',$Max_page);
+        return view('admin_layout')->with('admin.list_account', $manager_account);
+    }
+}
