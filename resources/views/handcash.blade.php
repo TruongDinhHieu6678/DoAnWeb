6a8ae7390eb8793406d6b0aa6f74ff7912fe5058
huynhcongly <50982883+huynhcongly@users.noreply.github.com>
2022-01-04 23:15:10 +0700

    Update handcash

---
+++ resources/views/handcash.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Supper | Shopper</title>
+    <link href="{{asset('public/frondend/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frondend/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frondend/css/prettyPhoto.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frondend/css/price-range.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frondend/css/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frondend/css/main.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frondend/css/responsive.css')}}" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+<![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png')}">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png')}">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png')}">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png')}">
+</head><!--/head-->
+
+
+<section id="cart_items">
+        <div class="container">
+            <div class="breadcrumbs">
+                <ol class="breadcrumb">
+                  <li><a href="{{URL::to('/trang-chu')}}">Trang chủ</a></li>
+                  </ol>
+                  <h2>Đơn hàng của bạn đã đặt thành công, chúng tôi sẽ liên lạc với bạn sớm nhất có thể!!!</h2>
+                  <br>
+                  <h2>Cám ơn bạn đã tin tưởng đặt hàng của dịch vụ chúng tôi</h2>
+                  <a href="#"><img src="public/frondend/images/Happy_new_year.JPG" width="700" height="500"></a>
+                  </div>
+                  </div>
+                  </section>
+
+</div>
+<script src="{{asset('public/frondend/js/jquery.js')}}"></script>
+<script src="{{asset('public/frondend/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('public/frondend/js/jquery.scrollUp.min.js')}}"></script>
+<script src="{{asset('public/frondend/js/price-range.js')}}"></script>
+<script src="{{asset('public/frondend/js/jquery.prettyPhoto.js')}}"></script>
+<script src="{{asset('public/frondend/js/main.js')}}"></script>
+<script src="{{asset('public/frondend/js/money.js')}}"></script>
+</body>
+</html>
